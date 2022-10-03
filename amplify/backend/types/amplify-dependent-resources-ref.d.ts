0eed18686154876b939bb48323660ad7934d69ed
Mathéo <matheobalens@gmail.com>
2022-10-03 14:39:12 +0200

    Structure de base

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "workshopa77308b7": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import {Box, Typography} from "@mui/material";
+
+
+export default function Home() {
+    return(
+        <Box>
+            <Typography>Page d'accueil</Typography>
+        </Box>
+    )
+}
